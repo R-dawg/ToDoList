c8472dbf2564a0e0bf9f7f4d5708b49376207196
Randall <ragrate@gmail.com>
2016-11-11 23:03:55 -0500

    Styling pt1 & pt2. Added stylings, colors, icons, and animation

--- assets/js/ToDo.js
+++ assets/js/ToDo.js
@@ -1,10 +1,21 @@
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+$("input[type = 'text']").keypress(function(event){
+	// if Enter is pressed
+	if(event.which === 13) {
+		// get new todo from input
+		var todoText = $(this).val();
+		// append to ul
+		$("ul").append("<li><span><i class='fa fa-ban'></i></span> " + todoText + "</li>")
+		$(this).val("");
+	}
 });
